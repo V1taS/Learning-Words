7a0e08720d1af874fad06e6bd89017b34bb054e9
Sosin Vitalii <375693@mail.ru>
2022-04-10 17:25:51 +0300

    Initial Commit

---
+++ Learning Words/Learning_WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_WordsApp.swift
+//  Learning Words
+//
+//  Created by Vitalii Sosin on 10.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
